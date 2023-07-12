ce086992afd48c21c54101c3b31b8a13ad3142e3
bheki-maenetja <bhekimaenetja@gmail.com>
2023-07-12 10:16:34 +0200

    Update models.py

--- jwt_auth/models.py
+++ jwt_auth/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    username = models.CharField(max_length=40, unique=True)
+
+    def __str__(self):
+        return f'{self.username} (Id: {self.id})'
