48a8b94e0ec10b68de60449c304342dff1dcfacb
bheki-maenetja <bhekimaenetja@gmail.com>
2023-07-15 08:55:39 +0200

    created model for docs

--- docs/models.py
+++ docs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Doc(models.Model):
+    title = models.CharField(max_length=10000)
+    content = models.CharField(max_length=1000000)
+    word_count = models.IntegerField(null=False, blank=False)
+    char_count = models.IntegerField(null=False, blank=False)
+    creation_date = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f"Id {self.id} | Title: {self.title}"
