51bde1a454b5a0f904749f2e679b07565b997e1c
bheki-maenetja <bhekimaenetja@gmail.com>
2023-07-26 11:08:00 +0200

    migrated from sqlite3 to postgres

--- project/settings.py
+++ project/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lame-db',
+        'USER': 'postgres',
+        'PASSWORD': 'Bonanz@4315',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
